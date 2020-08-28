dcc7ab20f6313c077d0223f6d14e9c49222551eb
JTTJ <JTTJ@Thomass-MacBook-Pro.local>
2020-08-27 23:52:58 -0500

    updated server.js

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.get("*", function(req, res) {
 });
 
 app.listen(PORT, function() {
-  console.log('🌎 ==> API server now on port ${PORT}!');
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
